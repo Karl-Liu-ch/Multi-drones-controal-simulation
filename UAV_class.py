6923486737b476eb7f7ba280c3f7f79f05870e5d
Karl-Liu-ch <57612675+Karl-Liu-ch@users.noreply.github.com>
2022-11-03 14:27:10 +0100

    Add files via upload

--- UAV_class.py
+++ UAV_class.py
@@ -14,38 +14,40 @@ class drone():
         self.t = t
         self.dt = 1e-2
     
-    def calculate_velocity(self):
+    def calculate_velocity(self, ax, ay, az):
+        self.ax, self.ay, self.az = ax, ay, az
         self.vx = self.vx + self.ax * self.dt
         self.vy = self.vy + self.ay * self.dt
         self.vz = self.vz + self.az * self.dt
-    
+        print(self.ax, self.ay, self.az)
+
     def calculate_position(self):
         self.x = self.x + self.vx * self.dt + 0.5 * self.ax * (self.dt ** 2)
         self.y = self.y + self.vy * self.dt + 0.5 * self.ay * (self.dt ** 2)
         self.z = self.z + self.vz * self.dt + 0.5 * self.az * (self.dt ** 2)
-    
+
     def update_t(self):
         self.t = self.t + self.dt
-        
-drone_1 = drone(0,0,0,0,0,0,5 * np.random.randn(),5 * np.random.randn(),5 * np.random.randn(),0)
 
-drones = [drone(0,0,0,0,0,0,5 * np.random.randn(),5 * np.random.randn(),5 * np.random.randn(),0) for k in range(5)]
+# drone_1 = drone(0,0,0,0,0,0,5 * np.random.randn(),5 * np.random.randn(),5 * np.random.randn(),0)
+#
+# drones = [drone(0,0,0,0,0,0,5 * np.random.randn(),5 * np.random.randn(),5 * np.random.randn(),0) for k in range(5)]
 
-px = [[] for k in range(len(drones))]
-py = [[] for k in range(len(drones))]
-pz = [[] for k in range(len(drones))]
-pt = [[] for k in range(len(drones))]
+# px = [[] for k in range(len(drones))]
+# py = [[] for k in range(len(drones))]
+# pz = [[] for k in range(len(drones))]
+# pt = [[] for k in range(len(drones))]
 
-print(px)
-for j in range(len(drones)):
-    drone_cur = drones[j]
-    for i in range(100):
-        drone_cur.calculate_velocity()
-        drone_cur.calculate_position()
-        drone_cur.update_t()
-        x, y, z, vx, vy, vz, t = drone_cur.x, drone_cur.y, drone_cur.z, drone_cur.vx, drone_cur.vy, drone_cur.vz, drone_cur.t
-        drone_cur = drone(x,y,z,vx,vy,vz,5 * np.random.randn(),5 * np.random.randn(),5 * np.random.randn(),t)
-        px[j].append(x)
-        py[j].append(y)
-        pz[j].append(z)
-        pt[j].append(t)
+# print(px)
+# for j in range(len(drones)):
+#     drone_cur = drones[j]
+#     for i in range(100):
+#         drone_cur.calculate_velocity()
+#         drone_cur.calculate_position()
+#         drone_cur.update_t()
+#         x, y, z, vx, vy, vz, t = drone_cur.x, drone_cur.y, drone_cur.z, drone_cur.vx, drone_cur.vy, drone_cur.vz, drone_cur.t
+#         drone_cur = drone(x,y,z,vx,vy,vz,5 * np.random.randn(),5 * np.random.randn(),5 * np.random.randn(),t)
+#         px[j].append(x)
+#         py[j].append(y)
+#         pz[j].append(z)
+#         pt[j].append(t)
--- test.py
+++ test.py
@@ -2,13 +2,13 @@ from matplotlib import pyplot as plt
 import numpy as np
 import mpl_toolkits.mplot3d.axes3d as p3
 from matplotlib import animation
-
+from UAV_class import drone
 
 fig = plt.figure()
 ax = p3.Axes3D(fig)
 
-q = [[-4.32, -2.17, -2.25, 4.72, 2.97, 1.74],
-     [ 2.45, 9.73,  7.45,4.01,3.42,  1.80],[-1.40, -1.76, -3.08,-9.94,-3.13,-1.13]]
+q = [[0, 1, 2, 3, 4, 5],
+     [ 0, 0, 0, 0, 0, 0],[0, 0, 0, 0, 0, 0]]
 v = [[ 0.0068,0.024, -0.014,-0.013, -0.0068,-0.04],[ 0.012,
       0.056, -0.022,0.016,  0.0045, 0.039],
      [-0.0045,  0.031,  0.077,0.0016, -0.015,-0.00012]]
@@ -21,31 +21,37 @@ u=np.array(v[1])
 w=np.array(v[2])
 
 ax.legend()
-ax.set_xlim([-1.5, 1.5])
-ax.set_ylim([-1.5, 1.5])
-ax.set_zlim([-1.5, 1.5])
 
-points, = ax.plot(x, y, z, '*')
+points, = ax.plot(x, y, z, 'o')
 txt = fig.suptitle('')
 
+drones = [drone(x[k], y[k], z[k], 0, 0, 0, 1 * np.random.randn(), 1 * np.random.randn(), 1 * np.random.randn(), 0) for k in
+              range(6)]
+
 def update_points(t, x, y, z, points):
-    # txt.set_text('num={:d}'.format(t))
+    txt.set_text('num={:d}'.format(t))
+    new_x = []
+    new_y = []
+    new_z = []
+    for j in range(len(drones)):
+        drone_cur = drones[j]
+        drone_cur.calculate_velocity(1 * np.random.randn(),1 * np.random.randn(),1 * np.random.randn())
+        drone_cur.calculate_position()
+        drone_cur.update_t()
+        px, py, pz, vx, vy, vz, t = drone_cur.x, drone_cur.y, drone_cur.z, drone_cur.vx, drone_cur.vy, drone_cur.vz, drone_cur.t
+        drone_cur = drone(px,py,pz,vx,vy,vz,1 * np.random.randn(),1 * np.random.randn(),1 * np.random.randn(),t)
+        new_x.append(px)
+        new_y.append(py)
+        new_z.append(pz)
 
-    new_x = x + s * t
-    new_y = y + u * t
-    new_z = z + w * t
-    # print('t:', t)
-
-    # update properties
-    points.set_data([x[0],new_x[0]],[y[0],new_y[0]])
-    points.set_3d_properties([z[0],new_z[0]], 'z')
-
-    # return modified artists
-    return points
+    new_x, new_y, new_z = np.array(new_x), np.array(new_y), np.array(new_z)
+    points.set_data(new_x, new_y)
+    points.set_3d_properties(new_z, 'z')
+    return points, txt
 
 ani=animation.FuncAnimation(fig, update_points, frames=99, fargs=(x, y, z, points))
 
-# ax.set_xlabel("x [pc]")
-# ax.set_ylabel("y [pc]")
-# ax.set_zlabel('z [pc]')
+ax.set_xlabel("x [pc]")
+ax.set_ylabel("y [pc]")
+ax.set_zlabel('z [pc]')
 plt.show()
