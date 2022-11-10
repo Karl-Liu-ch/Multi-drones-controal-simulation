7612be4d9def29da759e223a50b43a1cd7b0da02
Ash <csabai.matyas@gmail.com>
2022-11-10 15:42:20 +0100

    added better visualization to traverse algorithm

--- 2d_traverse.py
+++ 2d_traverse.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import numpy as np
 import time
+import random
 
 DRONE_NUM = 6
 board = [[0 for k in range(DRONE_NUM * 3)], [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0]]
@@ -59,9 +60,55 @@ class Drone:
             if board[1][self.x] == 0: 
                 break
 
-def animate():
-    shownBoard.set_data(board)
-    return shownBoard
+    def moveRight(self):
+        if (drones.index(self) < len(drones)-1 and drones[drones.index(self)+1].x == self.x+1):
+            drones[drones.index(self)+1].moveRight()
+        self.x += 1
+
+    def moveLeft(self):
+        if (drones.index(self) > 0 and drones[drones.index(self)-1].x == self.x-1):
+            drones[drones.index(self)-1].moveLeft()
+        self.x -= 1
+
+    def reset(self):
+        while True:
+            if self.x == drones.index(self)*3 + 1:
+                break
+            if self.x < drones.index(self)*3 + 1:
+                self.moveRight()
+            elif self.x > drones.index(self)*3 + 1:
+                self.moveLeft()
+
+def animate(i):
+    if i%4 == 0:
+        for d in drones:
+            d.move()
+    if i%4 == 1:
+        moveObs()
+    if i%4 == 2:
+        moveObs()
+        newObs()        
+    if i%4 == 3:
+        for d in drones:
+            d.reset()
+    updateBoard()
+
+    return [shownBoard]
+
+def newObs():
+    obs_num = random.randint(0, len(board[0]) - len(drones))
+    obstacles = random.sample(range(len(board[1])), obs_num)
+    for o in obstacles:
+        board[1][o] = 2
+
+def moveObs():
+    for i in range(len(board[0])):
+        if board[0][i] > 1:
+            board[0][i] = 0
+        if board[1][i] > 0:
+            board[0][i] = board[1][i]
+            board[1][i] = 0
+        
 
 def updateBoard():
     for i in range(len(board)):
@@ -74,28 +121,23 @@ def updateBoard():
         else:
             board[0][d.x] = 1
     shownBoard.set_data(board)
-    plt.draw()
     
 def getDrone(x):
     for d in drones:
         if d.x == x:
             return d
 
-#plt.show()
 
-
-shownBoard = plt.imshow(board)
 
 drones = []
 for i in range(DRONE_NUM):
     drones.append(Drone( i*3+1, i))
 
-updateBoard()
-
-for d in drones:
-    d.move()
-
-updateBoard()
+fig = plt.figure()
+shownBoard = plt.imshow(board)
+#plt.show()
+anim = animation.FuncAnimation(fig, animate, frames = 20, interval = 200, blit = True)
+print("done")
 plt.show()
 
 
