94cdcfd85426f9a1f96a5be391398f814a9c9cd2
Karl-Liu-ch <liu_ht2010@qq.com>
2022-11-24 12:54:37 +0100

    resules

--- 3D_VO/Results_analysis.py
+++ 3D_VO/Results_analysis.py
@@ -2,14 +2,14 @@ import sys
 sys.path.append('../')
 from UAV_size_changeable_3d import *
 Results = {}
-for i in range(4):
+for i in range(10):
     DETECT_NOISE = (i) * 0.02
-    for j in range(4):
+    for j in range(5):
         update_frequency = j + 1
-        for k in range(7):
+        for k in range(10):
             Safe_Threshold = 1 + 0.1 * (k + 1)
             PATH = 'UAVs{}_OBS{}_DN{}_UF{}_ST{}/'.format(4, 8, DETECT_NOISE, update_frequency, Safe_Threshold)
             robot_state_history, obs_state_history, robots_radius, obs_radius, obs_heights, results = load_uavs_obs(
                 PATH=PATH)
-            Results['DN{}_UF{}_ST{}'.format(DETECT_NOISE, update_frequency, Safe_Threshold)] = results
+            Results['DN{}_UF{}_ST{}'.format(DETECT_NOISE, update_frequency, Safe_Threshold)] = results[:]
 print(Results)
